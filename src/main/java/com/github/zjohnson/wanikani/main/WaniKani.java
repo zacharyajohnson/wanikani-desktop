8188c73100d406f75168f939bdf621c78a894126
Zachary Alan Johnson <zjohnson366@gmail.com>
2020-12-14 02:04:03 -0500

    Logic for h2 local db
    Logic to create a h2 database on the users computer under their user
    home folder. Logic as well for liquibase to update h2 db as I add
    further changesets. Liquibase will run an update each time the app is
    run.

--- src/main/java/com/github/zjohnson/wanikani/main/WaniKani.java
+++ src/main/java/com/github/zjohnson/wanikani/main/WaniKani.java
@@ -2,11 +2,42 @@ package com.github.zjohnson.wanikani.main;
 
 import com.github.zjohnson.wanikani.gui.login.LoginStage;
 import javafx.application.Application;
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import org.h2.jdbcx.JdbcDataSource;
 import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class WaniKani extends Application {
 
     public static void main(String[] args) {
+        String homeDirectory = System.getProperty("user.home");
+        JdbcDataSource ds = new JdbcDataSource();
+        ds.setURL("jdbc:h2:" + homeDirectory + "/.h2/wanikani-desktop/wanikani-desktop");
+        ds.setUser("sa");
+        ds.setPassword("sa");
+
+        try {
+            Connection conn = ds.getConnection();
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(conn));
+            Liquibase liquibase = new Liquibase("h2/liquibase/db/changesets/dbchangelog.xml", new ClassLoaderResourceAccessor(),database);
+            liquibase.update(new Contexts(), new LabelExpression());
+
+        } catch (SQLException | DatabaseException throwables) {
+            throwables.printStackTrace();
+        } catch (LiquibaseException e) {
+            e.printStackTrace();
+        }
+
         launch();
     }
 
