7b52f785318f7242426f29dc0103fbc99b31eaa8
Zachary Alan Johnson <zjohnson366@gmail.com>
2021-05-15 21:56:20 -0400

    Create mybatis mapper xml file and DAO/service classes for User

---
+++ src/main/java/dev/zacharyajohnson/wanikani/desktop/dao/UserDAO.java
@@ -0,0 +1,11 @@
+package dev.zacharyajohnson.wanikani.desktop.dao;
+
+
+import dev.zacharyajohnson.wanikani.desktop.model.User;
+import org.apache.ibatis.annotations.*;
+
+public interface UserDAO {
+    User getUser(@Param("id") String id);
+
+    void createUser(User user);
+}
---
+++ src/main/java/dev/zacharyajohnson/wanikani/desktop/service/UserService.java
@@ -0,0 +1,28 @@
+package dev.zacharyajohnson.wanikani.desktop.service;
+
+
+import dev.zacharyajohnson.wanikani.desktop.dao.UserDAO;
+import dev.zacharyajohnson.wanikani.desktop.dao.common.sql.session.WaniKaniSqlSessionFactory;
+import dev.zacharyajohnson.wanikani.desktop.model.User;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+public class UserService {
+
+    private SqlSessionFactory  sqlSessionFactory = WaniKaniSqlSessionFactory.getInstance();
+
+    public User getUserBy(String id) {
+        try(SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            UserDAO userDAO = sqlSession.getMapper(UserDAO.class);
+            return userDAO.getUser(id);
+        }
+    }
+
+    public void createUser(User user) {
+        try(SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            UserDAO userDAO = sqlSession.getMapper(UserDAO.class);
+            userDAO.createUser(user);
+            sqlSession.commit();
+        }
+    }
+}
